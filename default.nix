be8f329ec392c6eb873a96451691a6add173510b
Andrew McMiddlin <andrew@ajmccluskey.com>
2020-11-17 21:08:54 +1000

    Use reveal.js in nix store, not unpkg.

--- default.nix
+++ default.nix
@@ -33,7 +33,7 @@ in
           $src/slides/features.md \
           $src/slides/references.md \
           > slides.md
-      pandoc -i -t revealjs --slide-level=2 --template=$src/template.revealjs --variable=codedir:$out --variable=transition:none --no-highlight -s slides.md -o index.html
+      pandoc -i -t revealjs --variable=revealjs-url:$out/reveal.js --slide-level=2 --template=$src/template.revealjs --variable=transition:none --no-highlight -s slides.md -o index.html
       rm slides.md
     '';
 
